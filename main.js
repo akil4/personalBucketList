3938e2b6b3470b68dadb4a8a9083d47bdf56ea97
akil4 <akils4388@gmail.com>
2025-01-27 07:03:27 +0530

    add js function to add new input in new li element

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let newitem = document.getElementById('new-item');
 let bucketlist = document.getElementById('bucket-list');
-let inputlist = [];
+let inputlist = JSON.parse(localStorage.getItem('inputlist')) || [];
 
 newitem.addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         let value = newitem.value;
         if (value) {
             inputlist.push(value);
+            localStorage.setItem('inputlist', JSON.stringify(inputlist));
             newitem.value = '';
             displayList();
         }
@@ -15,5 +16,11 @@ newitem.addEventListener('keypress', function(event) {
 
 function displayList() {
     bucketlist.innerHTML = '';
-    let ul = document.create
+    let ol = document.createElement('ol');
+    inputlist.forEach(item => {
+        let li = document.createElement('li');
+        li.textContent = item;
+        ol.appendChild(li);
+    });
+    bucketlist.appendChild(ol);
 }
